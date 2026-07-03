4ddfd068df26a4e37629171d1d0ce1e0b6f03807
rajagceian <raja.gce.gce@gmail.com>
2026-07-03 14:13:13 +0530

    Add Count of Smaller Numbers After Self algorithm implementation

---
+++ Sorting-Algorithm/countInversion.cpp
@@ -0,0 +1,46 @@
+//Link => https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+// ============ T.C = O(nlogn) and S.C = O(n) ============
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void merge(vector<pair<int,int>>& arr,int l,int mid,int h,vector<int>& cnt){
+        vector <pair <int,int>> temp(h-l+1);
+        int i = l;
+        int j = mid + 1;
+        int idx = 0;
+        while(i<=mid && j<=h){
+            if(arr[i].first <= arr[j].first){
+                cnt[arr[i].second] += (j-(mid+1)); //(j-(mid+1)) => How many elements from the right half have already been placed into the merged array before the current left element
+                temp[idx++] = arr[i++];
+            } else{ 
+                temp[idx++] = arr[j++];
+            }
+        }
+        while(i<=mid){
+            cnt[arr[i].second] += (j-(mid+1));
+            temp[idx++] = arr[i++];
+        }
+        while(j<=h){
+            temp[idx++] = arr[j++];
+        }
+        for(int k=l;k<=h;k++){
+            arr[k] = temp[k-l];
+        }
+    }
+    void mergeSort(vector<pair<int,int>>& arr,int l,int h,vector<int>& cnt){
+        if(l>=h) return;
+        int mid = l + (h-l)/2;
+        mergeSort(arr,l,mid,cnt);
+        mergeSort(arr,mid+1,h,cnt);
+        merge(arr,l,mid,h,cnt);
+    }
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        vector <pair <int,int>> arr;
+        for(int i=0;i<n;i++) arr.push_back({nums[i],i});
+        vector <int> cnt(n);
+        mergeSort(arr,0,n-1,cnt);
+        return cnt;
+    }
+};
